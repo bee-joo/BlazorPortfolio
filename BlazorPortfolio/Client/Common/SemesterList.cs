a54b414227e7e6baff84fdd178681da9598128c0
bee-joo <bukin-danila@yandex.ru>
2022-12-27 19:14:03 +0300

    Add CustomUserFactory

---
+++ BlazorPortfolio/Client/Common/SemesterList.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorPortfolio.Client.Common
+{
+    public class SemesterList : List<int>
+    {
+        public SemesterList()
+        {
+            for (int i = 3; i <= 7; i++)
+            {
+                Add(i);
+            }
+        }
+    }
+}
